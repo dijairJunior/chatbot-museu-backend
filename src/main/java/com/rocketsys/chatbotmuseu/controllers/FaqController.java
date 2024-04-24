df7d06dea5ce3115b9af19a4a5f715bf969d02ed
Dijair Camargo <dijair.camargo@gmail.com>
2024-04-24 08:20:04 -0300

    refactor: change Faqervice to final

--- src/main/java/com/rocketsys/chatbotmuseu/controllers/FaqController.java
+++ src/main/java/com/rocketsys/chatbotmuseu/controllers/FaqController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/chat")
 public class FaqController {
 
-    private FaqService faqService;
+    final private FaqService faqService;
 
     private FaqController(FaqService faqService){
         this.faqService = faqService;
